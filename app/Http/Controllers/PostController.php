b3aaa4577324522dc6eec0a35d1089a525f056b1
frankkeybet <94977450+frankkeybet@users.noreply.github.com>
2025-05-23 07:00:26 +0300

    Eager Loading to query data faster and load our project faster, Updated the post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-       $posts = Post::all();
+       $posts = Post::with(['user', 'comments'])->get();
 
          return view('admin.posts.index', compact('posts'));
 
